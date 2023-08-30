da111f8bc4378c3e9f134a364e0e9588d3a81e9d
赵万梓 <zhao3eric@gmail.com>
2023-08-30 15:34:25 +0800

    [feat] 异常提醒

--- xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/XxlJobAdminApplication.java
@@ -1,5 +1,6 @@
 package com.xxl.job.admin;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author xuxueli 2018-10-28 00:38:13
  */
 @SpringBootApplication
+@EnableDubbo
 public class XxlJobAdminApplication {
 
 	public static void main(String[] args) {
---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/alarm/impl/AlfredAlarm.java
@@ -0,0 +1,43 @@
+package com.xxl.job.admin.core.alarm.impl;
+
+import com.loctek.internalservice.alfred.remote.AlarmRemoteService;
+import com.xxl.job.admin.core.alarm.JobAlarm;
+import com.xxl.job.admin.core.model.XxlJobInfo;
+import com.xxl.job.admin.core.model.XxlJobLog;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * @author: 赵万梓
+ */
+@Component
+public class AlfredAlarm implements JobAlarm {
+    private static Logger log = LoggerFactory.getLogger(EmailJobAlarm.class);
+
+    @DubboReference(group = "loctek-alfred", check = false)
+    private AlarmRemoteService alarmRemoteService;
+
+    @Override
+    public boolean doAlarm(XxlJobInfo info, XxlJobLog jobLog) {
+        Date time = Optional.ofNullable(jobLog.getHandleTime()).orElse(jobLog.getTriggerTime());
+        String message = Optional.ofNullable(jobLog.getHandleMsg()).orElse(jobLog.getTriggerMsg());
+        try {
+            String empNoStr = info.getAlarmEmail();
+            if (StringUtils.isBlank(empNoStr)){
+                return true;
+            }
+            String[] empNos = empNoStr.split(",");
+            alarmRemoteService.alarm(jobLog.getExecutorHandler(),String.valueOf(jobLog.getId()),time, Arrays.asList(empNos),message);
+        } catch (Exception e) {
+            log.error("消息发送失败",e);
+        }
+        return true;
+    }
+}
